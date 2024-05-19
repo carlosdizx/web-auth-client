e8cda841704392f935b4c995e628c1fa11ff2be0
ernesto diaz <ernesto.diaz@miliopay.com>
2024-05-18 20:38:46 -0500

    :sparkles: create function for register device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {client, server} from '@passwordless-id/webauthn'
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
 })
 export default class AppComponent {
+
+  private readonly challenge = "a7c61ef9-dc23-4806-b486-2428938a547e"
+
+  public register = {}
+  public serverRegister = {}
+
+  public registerDevice = async () => {
+    try {
+      const registrationEncoded = await client.register("Milio", this.challenge, {
+        "authenticatorType": "auto",
+        "userVerification": "required",
+        "discoverable": "required",
+        "timeout": 60000,
+        "attestation": true,
+      })
+
+      this.register = registrationEncoded//
+
+      const expected = {
+        challenge: this.challenge,
+        origin: "http://localhost:4200",
+      }
+      this.serverRegister = await server.verifyRegistration(registrationEncoded, expected)
+    }
+    catch (error) {
+      alert("Error: " + error)
+      throw new Error(error as string)
+    }
+  }
+  protected readonly JSON = JSON;
 }
